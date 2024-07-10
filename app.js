e0363a6391f48f95260f39fa815c41396ed3738b
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-07-10 10:42:52 -0600

    Refactor into functions
    Refactor the db queries into stand alone functions so that we can restructure things.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { parseBands } from "./parsers/bands.js";
 import { parseChromosomes } from "./parsers/chromosomes.js";
 import { parseHg19Centromeres, parseHg38Centromeres } from "./parsers/centromeres.js";
 import { annotateJson, vcfToJson } from "./testing/annotate_json.js";
-import { getGeneIdsList } from "./testing/dbHelpers.js";
+import { getGeneIdsList, getOverlappedGenes, getGeneAssociations } from "./testing/dbHelpers.js";
 import sqlite3 from 'sqlite3';
 import express from 'express';
 
@@ -135,7 +135,7 @@ app.get('/genes', (req, res) => {
     });
 });
 
-app.get('/genes/region', (req, res) => {
+app.get('/genes/region', async (req, res) => {
     let build = req.query.build;
     let source = req.query.source;
     let startChr = req.query.startChr
@@ -150,150 +150,11 @@ app.get('/genes/region', (req, res) => {
         return;
     }
 
-    if (source === 'refseq') {
-        sourceText = ' AND source = "refseq"';
-    }
-
-    let query = '';
-    let buildText = '';
-
-    if (build === 'hg19') {
-        buildText = 'build = "GRCh37"';
-    } else if (build === 'hg38') {
-        buildText = 'build = "GRCh38"';
-    }
-
-    let params = [];
-
-    if (startChr == endChr) {
-        params = [startChr, startPos, endPos, startPos, startPos, endPos, endPos]
-        query = `SELECT * FROM genes WHERE ${buildText} AND chr = ? AND ((start >= ? AND end <= ?) OR ((? >= start AND ? < end) OR (? >= start AND ? < end)))` + sourceText;
-    } else if (between.length > 0) {
-        let placeholders = between.map(() => '?').join(', ')
-        params = [startChr, startPos, startPos, endChr, endPos, endPos, ...between]
-        query = `SELECT * FROM genes WHERE ${buildText} AND ((chr = ? AND (start >= ? OR end >= ?)) OR (chr = ? AND (end <= ? OR start <= end ?)) OR (chr IN (${placeholders})))` + sourceText;   
-    } else {
-        params = [startChr, startPos, startPos, endChr, endPos, endPos]
-        query = `SELECT * FROM genes WHERE ${buildText} AND ((chr = ? AND (start >= ? OR end >= ?)) OR (chr = ? AND (end <= ? OR start <= end ?))` + sourceText;
-    }
-
     const db = new sqlite3.Database('./data/geneinfo.db/gene.iobio.db');
 
-    db.all(query, params, (err, rows) => {
-        db.close(); // Close the database after fetching the data
-
-        if (err) {
-            res.status(500).send(err.message);
-            return;
-        }
-        //for each row we want to have this become a json object where the gene_symbol is the key
-        let geneMap = {};
-        rows.forEach(row => {
-            geneMap[row.gene_symbol] = row;
-        });
-        res.send(geneMap);
-    });
-})
-
-//We will also want to get phenotypes for a given gene
-app.get('/genePhenotypes', (req, res) => {
-    let geneName = req.query.gene;
-    let geneNum = null;
-
-    if (!geneName) {
-        res.status(400).send('gene is a required parameter for this endpoint');
-        return;
-    }
-
-    //We need to get the gene # from our genes table to use in other queries
-    const db = new sqlite3.Database('./data/hpo.db');
-    let geneQuery = 'SELECT * FROM genes WHERE gene_symbol = ?';
-    let geneParams = [geneName];
-
-    let phenQuery = ''
-
-    db.all(geneQuery, geneParams, (err, rows) => {
-        if (err) {
-            res.status(500).send(err.message);
-            return;
-        }
-
-        //There should only be one gene that matches a particular gene symbol if there aren't any dont continue
-        if (rows.length == 0) {
-            res.send([])
-            return;
-        }
-
-        //If we get here we had a gene found with that symbol
-        geneNum = rows[0].gene_id;
-        phenQuery = `SELECT * FROM term_to_gene WHERE gene_id = ${geneNum}` //not washing because this came from our own db
-
-        db.all(phenQuery, [], (err, rows) => {
-            db.close();
-    
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-    
-            let phenMap = {}
-            rows.forEach(row => {
-                phenMap[row.term_id] = row;
-            })
-            res.send(phenMap)
-        });
-    })
-
-})
-
-//We will also want to get phenotypes for a given gene
-app.get('/geneDiseases', (req, res) => {
-    let geneName = req.query.gene;
-    let geneNum = null;
-
-    if (!geneName) {
-        res.status(400).send('gene is a required parameter for this endpoint');
-        return;
-    }
-
-    //We need to get the gene # from our genes table to use in other queries
-    const db = new sqlite3.Database('./data/hpo.db');
-    let geneQuery = 'SELECT * FROM genes WHERE gene_symbol = ?';
-    let geneParams = [geneName];
-
-    let diseaseQuery = ''
-
-    db.all(geneQuery, geneParams, (err, rows) => {
-        if (err) {
-            res.status(500).send(err.message);
-            return;
-        }
-
-        //There should only be one gene that matches a particular gene symbol if there aren't any dont continue
-        if (rows.length == 0) {
-            res.send([])
-            return;
-        }
-
-        //If we get here we had a gene found with that symbol
-        geneNum = rows[0].gene_id;
-        diseaseQuery = `SELECT * FROM gene_to_disease WHERE gene_id = ${geneNum}`//not washing because this came from our own db
-
-        db.all(diseaseQuery, [], (err, rows) => {
-            db.close();
-    
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-    
-            let diseaseMap = {}
-            rows.forEach(row => {
-                diseaseMap[row.disease_id] = row;
-            });
-            res.send(diseaseMap)
-        });
-    })
+    let geneMap = await getOverlappedGenes(build, source, startChr, startPos, endChr, endPos, between, sourceText, db);
+    db.close();
+    res.send(geneMap);
 })
 
 //We will also want to get phenotypes for a given gene
@@ -306,61 +167,10 @@ app.get('/geneAssociations', async (req, res) => {
     }
 
     const db = new sqlite3.Database('./data/hpo.db');
-    let geneIdsList = await getGeneIdsList(genes, db);
-
-    let placeholders = geneIdsList.map(() => '?').join(',');
-
-    let phenQuery = `SELECT term_to_gene.*, genes.gene_symbol, terms.name 
-        FROM term_to_gene 
-        JOIN genes ON term_to_gene.gene_id = genes.gene_id
-        JOIN terms ON term_to_gene.term_id = terms.term_id
-        WHERE term_to_gene.gene_id IN (${placeholders})`;
-    let phenPromise = new Promise((resolve, reject) => {
-        db.all(phenQuery, geneIdsList, (err, rows) => {
-            if (err) {
-                reject(err);
-            }
-    
-            let phenotypesToGene = {};
-            rows.forEach(row => {
-                if (phenotypesToGene.hasOwnProperty(row.gene_symbol)){
-                    phenotypesToGene[row.gene_symbol][row.term_id] = row;
-                } else {
-                    //establish the structure of the phenotypes to gene
-                    phenotypesToGene[row.gene_symbol] = {};
-                    phenotypesToGene[row.gene_symbol][row.term_id] = row;
-                }
-            })
-            resolve(phenotypesToGene);
-        });
-    })
-
-    let diseaseQuery = `
-        SELECT gene_to_disease.*, genes.gene_symbol, diseases.disease_name 
-        FROM gene_to_disease 
-        JOIN genes ON gene_to_disease.gene_id = genes.gene_id
-        JOIN diseases ON gene_to_disease.disease_id = diseases.disease_id 
-        WHERE gene_to_disease.gene_id IN (${placeholders})`
-    let diseasePromise = new Promise((resolve, reject) => {
-        db.all(diseaseQuery, geneIdsList, (err, rows) => {
-            if (err) {
-                reject(err);
-            }
-            let diseasesToGene = {};
-            rows.forEach(row => {
-                if (diseasesToGene.hasOwnProperty(row.gene_symbol)){
-                    diseasesToGene[row.gene_symbol][row.disease_id] = row;
-                } else {
-                    diseasesToGene[row.gene_symbol] = {};
-                    diseasesToGene[row.gene_symbol][row.disease_id] = row;
-                }
-            })
-            resolve(diseasesToGene);
-        });
-    })
-    let [phenotypeMap, diseaseMap] = await Promise.all([phenPromise, diseasePromise]);
+    const { phenToGene, diseaseToGene } = await getGeneAssociations(genes, db);
     db.close();
-    res.send({phenToGene: phenotypeMap, diseaseToGene: diseaseMap})
+    
+    res.send({phenToGene, diseaseToGene});
 })
 
 app.get('/phenotypeGenes', (req, res) => {
--- testing/dbHelpers.js
+++ testing/dbHelpers.js
@@ -16,3 +16,121 @@ export function getGeneIdsList(geneNamesList, db) {
         })
     })
 }
+
+export function getGeneAssociations(genes, db) {
+    /**
+     * 
+     */
+    return new Promise((resolve, reject) => {
+        getGeneIdsList(genes, db).then(geneIdsList => {
+            let placeholders = geneIdsList.map(() => '?').join(',');
+
+            const getPhenotypes = () => {
+                return new Promise((resolve, reject) => {
+                    let phenQuery = `SELECT term_to_gene.*, genes.gene_symbol, terms.name 
+                        FROM term_to_gene 
+                        JOIN genes ON term_to_gene.gene_id = genes.gene_id
+                        JOIN terms ON term_to_gene.term_id = terms.term_id
+                        WHERE term_to_gene.gene_id IN (${placeholders})`;
+
+                    db.all(phenQuery, geneIdsList, (err, rows) => {
+                        if (err) {
+                            reject(err);
+                        }
+                
+                        let phenotypesToGene = {};
+                        rows.forEach(row => {
+                            if (phenotypesToGene.hasOwnProperty(row.gene_symbol)){
+                                phenotypesToGene[row.gene_symbol][row.term_id] = row;
+                            } else {
+                                //establish the structure of the phenotypes to gene
+                                phenotypesToGene[row.gene_symbol] = {};
+                                phenotypesToGene[row.gene_symbol][row.term_id] = row;
+                            }
+                        })
+                        resolve(phenotypesToGene);
+                    });
+                })
+            };
+
+            const getDiseases = () => {
+                return new Promise((resolve, reject) => {
+                    let diseaseQuery = `
+                        SELECT gene_to_disease.*, genes.gene_symbol, diseases.disease_name 
+                        FROM gene_to_disease 
+                        JOIN genes ON gene_to_disease.gene_id = genes.gene_id
+                        JOIN diseases ON gene_to_disease.disease_id = diseases.disease_id 
+                        WHERE gene_to_disease.gene_id IN (${placeholders})`
+    
+                    db.all(diseaseQuery, geneIdsList, (err, rows) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        let diseasesToGene = {};
+                        rows.forEach(row => {
+                            if (diseasesToGene.hasOwnProperty(row.gene_symbol)){
+                                diseasesToGene[row.gene_symbol][row.disease_id] = row;
+                            } else {
+                                diseasesToGene[row.gene_symbol] = {};
+                                diseasesToGene[row.gene_symbol][row.disease_id] = row;
+                            }
+                        })
+                        resolve(diseasesToGene);
+                    });
+                })
+            };
+
+            Promise.all([getPhenotypes(), getDiseases()]).then(([phenotypeMap, diseaseMap]) => {
+                resolve({phenToGene: phenotypeMap, diseaseToGene: diseaseMap});
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    });
+}
+
+export function getOverlappedGenes(build, source, startChr, startPos, endChr, endPos, between, sourceText, db) {
+    /**
+     * 
+     */
+    return new Promise((resolve, reject) => {
+        if (source === 'refseq') {
+            sourceText = ' AND source = "refseq"';
+        }
+    
+        let query = '';
+        let buildText = '';
+    
+        if (build === 'hg19') {
+            buildText = 'build = "GRCh37"';
+        } else if (build === 'hg38') {
+            buildText = 'build = "GRCh38"';
+        }
+    
+        let params = [];
+    
+        if (startChr == endChr) {
+            params = [startChr, startPos, endPos, startPos, startPos, endPos, endPos]
+            query = `SELECT * FROM genes WHERE ${buildText} AND chr = ? AND ((start >= ? AND end <= ?) OR ((? >= start AND ? < end) OR (? >= start AND ? < end)))` + sourceText;
+        } else if (between.length > 0) {
+            let placeholders = between.map(() => '?').join(', ')
+            params = [startChr, startPos, startPos, endChr, endPos, endPos, ...between]
+            query = `SELECT * FROM genes WHERE ${buildText} AND ((chr = ? AND (start >= ? OR end >= ?)) OR (chr = ? AND (end <= ? OR start <= end ?)) OR (chr IN (${placeholders})))` + sourceText;   
+        } else {
+            params = [startChr, startPos, startPos, endChr, endPos, endPos]
+            query = `SELECT * FROM genes WHERE ${buildText} AND ((chr = ? AND (start >= ? OR end >= ?)) OR (chr = ? AND (end <= ? OR start <= end ?))` + sourceText;
+        }
+    
+        db.all(query, params, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            //for each row we want to have this become a json object where the gene_symbol is the key
+            let geneMap = {};
+            rows.forEach(row => {
+                geneMap[row.gene_symbol] = row;
+            });
+            resolve(geneMap);
+        });
+    });
+}
